dd31bad7fa0a49484aee498e168f32b6920dd738
Adam Vandolder <adam.vandolder@gmail.com>
2020-01-12 17:19:27 -0500

    Add chapter 3 contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod vec3;
+
+use vec3::Vec3;
+
 fn main() {
     let nx = 200;
     let ny = 100;
@@ -5,11 +9,11 @@ fn main() {
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let (r, g, b) = (i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
+            let color = Vec3(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
             let (ir, ig, ib) = (
-                (255.99 * r) as i32,
-                (255.99 * g) as i32,
-                (255.99 * b) as i32,
+                (255.99 * color.0) as i32,
+                (255.99 * color.1) as i32,
+                (255.99 * color.2) as i32,
             );
             println!("{} {} {}", ir, ig, ib);
         }
---
+++ src/vec3.rs
@@ -0,0 +1,140 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Vec3(pub f32, pub f32, pub f32);
+
+impl Vec3 {
+    pub fn x(&self) -> f32 { self.0 }
+    pub fn y(&self) -> f32 { self.1 }
+    pub fn z(&self) -> f32 { self.2 }
+    pub fn r(&self) -> f32 { self.0 }
+    pub fn g(&self) -> f32 { self.1 }
+    pub fn b(&self) -> f32 { self.2 }
+
+    pub fn length(&self) -> f32 {
+        (self.0*self.0 + self.1*self.1 + self.2*self.2).sqrt()
+    }
+
+    pub fn squared_length(&self) -> f32 {
+        (self.0*self.0 + self.1*self.1 + self.2*self.2)
+    }
+
+    pub fn make_unit_vector(&mut self) {
+        let k = 1. / self.length();
+        self.0 *= k;
+        self.1 *= k;
+        self.2 *= k;
+    }
+
+    pub fn unit_vector(&self) -> Self {
+        *self / self.length()
+    }
+
+    pub fn dot(&self, other: &Self) -> f32 {
+        self.0*other.0 + self.1*other.1 + self.2*other.2
+    }
+
+    pub fn cross(&self, other: &Self) -> Self {
+        Vec3(
+            self.1*other.2 - self.2*other.1,
+            self.2*other.0 - self.0*other.2,
+            self.0*other.1 - self.1*other.0,
+        )
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Vec3(self.0 + other.0, self.1 + other.1, self.2 + other.2)
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        self.0 += other.0;
+        self.1 += other.1;
+        self.2 += other.2;
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Vec3(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        self.0 -= other.0;
+        self.1 -= other.1;
+        self.2 -= other.2;
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Vec3(self.0 * other.0, self.1 * other.1, self.2 * other.2)
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, other: Self) {
+        self.0 *= other.0;
+        self.1 *= other.1;
+        self.2 *= other.2;
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: f32) -> Self {
+        Vec3(self.0 * other, self.1 * other, self.2 * other)
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, other: f32) {
+        self.0 *= other;
+        self.1 *= other;
+        self.2 *= other;
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self {
+        Vec3(self.0 / other.0, self.1 / other.1, self.2 / other.2)
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, other: Self) {
+        self.0 /= other.0;
+        self.1 /= other.1;
+        self.2 /= other.2;
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: f32) -> Self {
+        Vec3(self.0 / other, self.1 / other, self.2 / other)
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, other: f32) {
+        self.0 /= other;
+        self.1 /= other;
+        self.2 /= other;
+    }
+}
